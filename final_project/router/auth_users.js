c414645de76b1678ddced723f45776434473b951
Maksimov86 <tylerbk666@gmail.com>
2024-05-24 20:09:40 +0700

    added code public_users.get to general.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,12 +9,12 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,7 +25,8 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
+  const author = req.params.author;
+  res.send(books[author])
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
